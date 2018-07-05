ca113bd84d8001bdaa043e44ceee27b6179a00a8
CasparOne <vvolodin.victor@gmail.com>
2018-07-05 15:54:14 +0300

    Autorisation form stage1

--- index.php
+++ index.php
@@ -1,7 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>PHP1</h1>
+
 <?php
-/**
- * Created by PhpStorm.
- * User: vvolo
- * Date: 04.07.2018
- * Time: 23:14
- */
+
+function maxi ($x, $y)
+{
+    if ($x > $y) {
+        $max = $x;
+    } else {
+        $max = $y;
+    }
+    return $max;
+}
+
+echo maxi(4,5);
+
+
+?>
+
+
+</body>
+</html>
