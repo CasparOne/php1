340f7f30bdc3fd61040151722f74821bfe7008bb
CasparOne <vvolodin.victor@gmail.com>
2018-07-06 13:25:34 +0300

    Home work 1 and 2 stage

--- functions.php
+++ functions.php
@@ -6,8 +6,18 @@
  * Time: 17:21
  */
 
-/** Function @n performs something */
-function n()
+/** Function @Discriminan calculates discriminant of quadratic equation */
+function Discriminant ($a, $b, $c)
 {
-
+    $D = (($b**2) - (4 * $a * $c));
+    return $D;
 }
+
+
+/** Assertation @Discriminant function -- only for Dev.
+ * In production should be removed */
+
+assert(16 === Discriminant(1, -8, 12));
+assert( -131 ===  Discriminant(5, 3, 7));
+assert(0 === Discriminant(1, -6, 9));
+
--- index.php
+++ index.php
@@ -8,19 +8,119 @@
     <title>Document</title>
 </head>
 <body>
-<h1>PHP1</h1>
+<h1>PHP1 - Урок 2 ДЗ</h1>
+<div><p>Таблица истинности "AND"</p>
+    <table border="1" title="">
+        <tr>
+            <td>A</td>
+            <td>B</td>
+            <td>A && B</td>
+        </tr>
+        <tr align="center">
+            <td>0</td>
+            <td>0</td>
+            <td><?php echo (int)(false && false); ?></td>
+        </tr>
+        <tr align="center">
+            <td>0</td>
+            <td>1</td>
+            <td><?php echo (int)(false && true); ?></td>
+        </tr>
+        <tr align="center">
+            <td>1</td>
+            <td>0</td>
+            <td><?php echo (int)(true && false); ?></td>
+        </tr>
+        <tr align="center">
+            <td>1</td>
+            <td>1</td>
+            <td><?php echo (int)(true && true); ?></td>
+        </tr>
+    </table>
+</div>
+
+<div><p>Таблица истинности "OR"</p>
+    <table border="1" title="">
+        <tr>
+            <td>A</td>
+            <td>B</td>
+            <td>A && B</td>
+        </tr>
+        <tr align="center">
+            <td>0</td>
+            <td>0</td>
+            <td><?php echo (int)(false || false); ?></td>
+        </tr>
+        <tr align="center">
+            <td>0</td>
+            <td>1</td>
+            <td><?php echo (int)(false || true); ?></td>
+        </tr>
+        <tr align="center">
+            <td>1</td>
+            <td>0</td>
+            <td><?php echo (int)(true || false); ?></td>
+        </tr>
+        <tr align="center">
+            <td>1</td>
+            <td>1</td>
+            <td><?php echo (int)(true || true); ?></td>
+        </tr>
+    </table>
+</div>
+
+<div><p>Таблица истинности "XOR"</p>
+    <table border="1" title="">
+        <tr>
+            <td>A</td>
+            <td>B</td>
+            <td>A && B</td>
+        </tr>
+        <tr align="center">
+            <td>0</td>
+            <td>0</td>
+            <td><?php echo (int)(false XOR false); ?></td>
+        </tr>
+        <tr align="center">
+            <td>0</td>
+            <td>1</td>
+            <td><?php echo (int)(false XOR true); ?></td>
+        </tr>
+        <tr align="center">
+            <td>1</td>
+            <td>0</td>
+            <td><?php echo (int)(true XOR false); ?></td>
+        </tr>
+        <tr align="center">
+            <td>1</td>
+            <td>1</td>
+            <td><?php echo (int)(true XOR true); ?></td>
+        </tr>
+    </table>
+</div>
+<br> <br>
 
 <?php
 include __DIR__ . '/functions.php';
 
-$a = 5;
-$b = false;
-const A = null ;
-const B = false;
+$a = 2;
+$b = 4;
+$c = 5;
+$D = Discriminant($a, $b, $c);
 
+echo '<br> <p>Решение квадратного уравнения</p> <br>' . $a . 'x^2+' . $b . 'x+' . $c . '<br>';
+echo 'a=' . $a . '<br>b=' . $b . '<br>c=' . $c . '<br>D=' . $D;
 
-var_dump(var_dump((int) A));
-//var_dump(var_dump((int) B));
+if ($D > 0) {
+    $x1 = ((-$b + sqrt($D)) / (2 * $a));
+    $x2 = ((-$b - sqrt($D)) / (2 * $a));
+    echo '<br>Корни уравнения: X1=' . $x1 . '  X2=' . $x2;
+} elseif ($D = 0) {
+    $x1 = ((-$b - sqrt($D)) / (2 * $a));
+    echo '<br>Корень уравнения: X=' . $x1;
+} else {
+    echo '<br> <p>Корней нет!</p>';
+}
 
 ?>
 
