00dd207e62c845040b9054fe8eb5369c6eeab181
CasparOne <vvolodin.victor@gmail.com>
2018-07-12 18:14:52 +0300

    Коммит промежуточный на конец дня.
    Signed-off-by: CasparOne <vvolodin.victor@gmail.com>

--- index.php
+++ index.php
@@ -1,35 +1,66 @@
 <?php
-session_start();
-if (!isset($_SESSION) || null == $_SESSION['usr']) {
-    $_SESSION['usr'] = null;
-    header('Location:http://php1.local/login.php');
+
+class TextFile
+{
+    protected $path;
+
+    public function __construct($path)
+    {
+        $this->path = $path;
+        if (!is_readable($this->path)) {
+            die();
+        }
+
+    }
+
 }
-include __DIR__ . '/functions.php';
-?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<h1>PHP1 Урок 5</h1>
-<h3>Вы вошли, как <?php echo ucfirst(getCurrentUser()); ?>!</h3>
-<form action="/upload.php" method="post" enctype="multipart/form-data">
-    <input type="file" name="myimage">
-    <button type="submit">Загрузить</button><br><br>
-</form>
-<?php
-$list = scandir(__DIR__ . '/images');
-$list = array_diff($list, ['.','..']);
-foreach ($list as $img) {
-    ?>
-    <img src="/images/<?php echo $img; ?>" height="200">
-    <?php
+
+class GuestBook extends TextFile
+{
+    protected $dataArr;
+    protected $text;
+
+    public function __construct($path)
+    {
+        parent::__construct($path);
+        $this->dataArr = file($this->path);
+    }
+
+    public function save()
+    {
+        $this->append($this->text);
+        foreach ($this->dataArr as $line) {
+            file_put_contents($this->path, $line);
+        }
+    }
+
+    public function append($text)
+    {
+        $this->text = $text;
+        $this->dataArr = $this->getData();
+        if (!isset($text)) {
+            die();
+        }
+        $this->dataArr[] = $text;
+        return $this->dataArr;
+    }
+
+    public function getData()
+    {
+        return $this->dataArr;
+    }
 }
+
+$text = 'Петров;Петр;Петрович;32345@ya.ru;7815003422';
+$path = __DIR__ . '/data.txt';
+
+$guestBook = new GuestBook($path);
+var_dump($guestBook->getData());
+$guestBook->append($text);
+$guestBook->save();
+var_dump($guestBook->getData());
+foreach ($guestBook->getData() as $line) {
+    ?> <h4><?php echo $line ?></h4><?php
+}
+
 ?>
-</body>
-</html>
