359c44ea1ea3cf472cd7a15ac8a1d1d83cf60278
CasparOne <vvolodin.victor@gmail.com>
2018-07-12 23:40:52 +0300

    0.01

--- index.php
+++ index.php
@@ -1,66 +1,54 @@
 <?php
 
+/** Родительский класс TextFile */
 class TextFile
 {
     protected $path;
+    public $readyData;
 
+    /** Подключает файл, путь которого - значение передаваемое как аргумент в конструктор */
     public function __construct($path)
     {
-        $this->path = $path;
-        if (!is_readable($this->path)) {
+        if (!is_readable($path)) {
             die();
         }
-
+        $this->path = $path;
     }
 
+    /** Сохраняет готовый контент в файл */
+    public function save()
+    {
+        file_put_contents($this->path, $this->readyData);
+    }
 }
 
+/** Класс GuestBook наследуется от класса TextFile */
 class GuestBook extends TextFile
 {
     protected $dataArr;
     protected $text;
 
-    public function __construct($path)
+    /** Получает значения в масив из файла */
+    public function getData()
     {
-        parent::__construct($path);
         $this->dataArr = file($this->path);
+        return $this->dataArr;
     }
 
-    public function save()
-    {
-        $this->append($this->text);
-        foreach ($this->dataArr as $line) {
-            file_put_contents($this->path, $line);
-        }
-    }
-
+    /** Добавляет новую элемент в массив. Возвращает готовый массив для записи в файл */
     public function append($text)
     {
         $this->text = $text;
-        $this->dataArr = $this->getData();
-        if (!isset($text)) {
-            die();
-        }
-        $this->dataArr[] = $text;
-        return $this->dataArr;
-    }
-
-    public function getData()
-    {
+        $this->getData();
+        $this->dataArr[] = "\n" . $text;
         return $this->dataArr;
     }
 }
 
-$text = 'Петров;Петр;Петрович;32345@ya.ru;7815003422';
+echo '<pre>';
 $path = __DIR__ . '/data.txt';
+$gb = new GuestBook($path);
+$gb->readyData = $gb->append('Петров;Инокентий;Филипович;d11114@yan223.ru;79153521244');
+$gb->save();
+var_dump($gb->getData());
 
-$guestBook = new GuestBook($path);
-var_dump($guestBook->getData());
-$guestBook->append($text);
-$guestBook->save();
-var_dump($guestBook->getData());
-foreach ($guestBook->getData() as $line) {
-    ?> <h4><?php echo $line ?></h4><?php
-}
-
-?>
