eabe5efdb41fc98b5070442a2421e22da259dd09
CasparOne <vvolodin.victor@gmail.com>
2018-07-20 00:12:45 +0300

    ООП Рулит!

--- upload.php
+++ upload.php
@@ -5,21 +5,17 @@ $formName = 'myimage';
 $fileSize = 5 * (2 ** 20) ; //размер файла 5 мегабат. Значение (int)5 можно поменять "по вкусу"
 $uploader = new Uploader($formName);
 
-
-
-
-
 if (isset($_FILES[$formName])) {
     if ($uploader->isUploaded() &&
         /** Дальше проверки разные, в след релизе все это будет проверяться внутри класса */
         ($_FILES[$formName]['type'] == 'image/jpeg' || $_FILES[$formName]['type'] == 'image/png') &&
         $_FILES[$formName]['size'] <=$fileSize) {
         $uploader->upload();
-        header('Location: http://php1.local/');
     }
     else {
         echo 'Error uploading image!';
         ?><br> <a href="/index.php"><< НАЗАД</a> <?php
     }
 }
+header('Location: http://php1.local/');
 
